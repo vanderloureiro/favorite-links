ea848cd84f7558e8cf32663566c6d0325c838f70
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2022-11-01 11:59:58 -0300

    estrutura de pacotes e encapsulamento

---
+++ src/main/java/com/br/favoritelinks/core/FavoriteLinksController.java
@@ -0,0 +1,7 @@
+package com.br.favoritelinks.core;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FavoriteLinksController {
+}
---
+++ src/main/java/com/br/favoritelinks/core/GroupRepository.java
@@ -0,0 +1,4 @@
+package com.br.favoritelinks.core;
+
+interface GroupRepository {
+}
