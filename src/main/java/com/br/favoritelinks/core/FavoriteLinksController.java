a90ede9f240bc768e52adf9255dc740eee431612
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2022-11-01 18:26:03 -0300

    conclusao de controller

--- src/main/java/com/br/favoritelinks/core/FavoriteLinksController.java
+++ src/main/java/com/br/favoritelinks/core/FavoriteLinksController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class FavoriteLinksController {
@@ -22,22 +23,45 @@ public class FavoriteLinksController {
 
     @PostMapping("/group")
     public ResponseEntity<Void> createGroup(@RequestBody GroupForm form) {
+        Group group = Group.builder().name(form.name()).build();
+        this.groupRepository.save(group);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/group")
     public ResponseEntity<List<GroupDto>> getAllGroups() {
-        return ResponseEntity.ok().build();
+        List<GroupDto> groups = this.groupRepository
+                .findAll()
+                .stream()
+                .map(group -> {
+                    return new GroupDto(group.getId(), group.getName());
+                }).collect(Collectors.toList());
+        return ResponseEntity.ok(groups);
     }
 
     @PostMapping("/link")
-    public ResponseEntity<LinkDto> createLink(@RequestBody LinkForm form) {
+    public ResponseEntity<Void> createLink(@RequestBody LinkForm form) {
+        Group group = this.groupRepository.findById(form.groupId()).orElseThrow();
+
+        Link link = Link.builder()
+                .description(form.description())
+                .link(form.link())
+                .group(group)
+                .build();
+
+        linkRepository.save(link);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/links-by-group/{id}")
     public ResponseEntity<List<LinkDto>> getAllLinksByGroup(@PathVariable Long groupId) {
-        return ResponseEntity.ok().build();
+        List<LinkDto> links = this.linkRepository
+                .getAllByGroupId(groupId)
+                .stream()
+                .map(link -> {
+                    return new LinkDto(link.getId(), link.getDescription(), link.getLink(), link.getGroup().getId());
+                }).collect(Collectors.toList());
+        return ResponseEntity.ok(links);
     }
 
 }
--- src/main/java/com/br/favoritelinks/core/LinkRepository.java
+++ src/main/java/com/br/favoritelinks/core/LinkRepository.java
@@ -3,6 +3,10 @@ package com.br.favoritelinks.core;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 interface LinkRepository extends JpaRepository<Link, Long> {
+
+    List<Link> getAllByGroupId(Long id);
 }
