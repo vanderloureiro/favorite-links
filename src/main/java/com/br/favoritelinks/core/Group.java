1dd41fd163056e8d357e380266600812173af11b
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2022-11-01 13:25:16 -0300

    criacao de entidades link e group

--- src/main/java/com/br/favoritelinks/core/Group.java
+++ src/main/java/com/br/favoritelinks/core/Group.java
@@ -1,7 +1,23 @@
 package com.br.favoritelinks.core;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Data
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
 }
--- src/main/java/com/br/favoritelinks/core/Link.java
+++ src/main/java/com/br/favoritelinks/core/Link.java
@@ -1,7 +1,26 @@
 package com.br.favoritelinks.core;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Data
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 class Link {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String link;
 }
